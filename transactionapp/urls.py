344aa1a70729873ef87743c8debbf6ca163d4a6d
OldStunna27 <168038090+OldStunna27@users.noreply.github.com>
2025-11-28 10:21:10 -0600

    first commit

--- transactionapp/urls.py
+++ transactionapp/urls.py
@@ -4,7 +4,7 @@ from userapp import views as user_views
 
 urlpatterns = [
     re_path(r'^loan_predict/(?P<user_id>\d+)/', transaction_views.loan_predict_view, name="loan_predict"),
-    # path('loan_result/<int:user_id>/', transaction_views.loan_result_view, name='loan_result'),
+    path('loan_result/<int:user_id>/', transaction_views.loan_result_view, name='loan_result'),
    
     
 
